0ace897c80b7da9e6cc780d9d427e72df51b7649
RoseMercy <mercyrose432@gmail.com>
2022-01-06 14:17:38 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask, request, session, redirect, render_template , g, url_for
 import pickle
-
+import os
 app = Flask(__name__)
 model = pickle.load(open(r'C:\Users\mercy\Downloads\Binge_trial\binge.pkl', 'rb'))
 @app.route('/')
@@ -20,6 +20,16 @@ def predict():
     
     return render_template('home.html', prediction_text='Your binge eating level is: {}'.format(prediction))
 
-
+#login
+#app.secret_key = os.urandom(24)
+#@app.route("/", methods= ['GET', 'POST'])
+#def home():
+    #if request.method == 'POST':
+       # session.pop('user', None)
+        
+       # if request.form['password'] ==' password':
+          #  session['user']
+        
+    
 if __name__ == "__main__":
     app.run(debug=True)
